49d36261f8f02e06dfbca69848dd3326278648d4
korgz <oskars.kubulis@gmail.com>
2022-06-14 20:13:30 +0300

    Add project files.

---
+++ ExceptionsDemo/NotFoundException.cs
@@ -0,0 +1,1 @@
+﻿namespace ExceptionsDemo;public class NotFoundException : Exception{    public NotFoundException()    {    }    public NotFoundException(string message) : base(message)    {    }    public NotFoundException(string message, Exception exception)        : base(message, exception)    {    }}
---
+++ ExceptionsDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using ExceptionsDemo;
+
+Console.WriteLine("Lūdzu ievadi skaitli?");
+string numberText = Console.ReadLine();
+
+try
+{
+    int userNumber = int.Parse(numberText);
+    Console.WriteLine($"Tu ievadīji skaitli: {userNumber}");
+}
+catch (FormatException ex)
+{
+    string exceptionMessage = ex.Message;
+
+    Console.WriteLine("Tas nebija skaitlis");
+    Console.WriteLine($"Apstrādes kļūda: {exceptionMessage}");
+}
+finally
+{
+ 
+    Console.WriteLine($"nosledz konekciju");
+}
+
+Console.WriteLine("Programmas beigas");
+
+Console.WriteLine();
+Console.WriteLine("==================");
+Console.WriteLine();
+
+
+Console.WriteLine("Ievadi tekstu?");
+string userText = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(userText))
+{
+    throw new NotFoundException("Lietotājs neievadīja tekstu");
+}
+else
+{
+    Console.WriteLine($"Tavs teksts: {userText}");
+}
